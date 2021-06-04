250c8c837f0b833b1b85031860a75cbb7fedcfc0
ChloeDumit <dumitchloe@gmail.com>
2021-06-04 19:17:05 +0000

    all

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Header from "./components/header/header";
+// import Header from "./components/header/header";
 import HeaderItems from "./components/header-items/header-items";
-import Home from "./components/home/home";
+// import Home from "./components/home/home";
 import ItemListContainer from "./components/itemListContainer/itemListContainer";
 import AllItems from "./components/AllItems/allItems";
 import Footer from "./components/footer/footer";
